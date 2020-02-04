0e49edbc42a41efdad94033f9cf08a5a6c726e4e
Evald Nexhipi <51530364+evaldnexhipi@users.noreply.github.com>
2020-02-04 23:40:17 +0100

    Update TOC.java

--- TOC.java
+++ TOC.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+
+import jdk.internal.org.objectweb.asm.tree.IntInsnNode;
 //Ɛ
 public class TOC {
 	
@@ -10,7 +13,7 @@ public class TOC {
 		//shtojme q0 e NFA tek Q0 i DFA
 		DFAState DFA = new DFAState ();
 		DFA.addTitle(NFA.get(0));
-		DFA.setType(Type.START);
+		DFA.setType(Tip.START);
 		Queue <DFAState> queue = new LinkedList<DFAState>();
 		queue.add(DFA);
 		DFASet.add(DFA);
@@ -54,16 +57,16 @@ public class TOC {
 		//ne momentin kur krijohet nje gjendje e re NFAState, nqs eClosure i saj shkon tek gjendja finale, athr Type.FINAL
 		HashSet <eNFAState> eNFAFinalStates = new HashSet<eNFAState>();
 		for (eNFAState enfa : eNFA) {
-			if (enfa.getType()==Type.FINAL) {
+			if (enfa.getType()==Tip.FINAL) {
 				eNFAFinalStates.add(enfa);
 			}
 		}
 		
 		for (eNFAState enfa : eNFA) {
-			Type t = enfa.getType();
+			Tip t = enfa.getType();
 			for (eNFAState finals : eNFAFinalStates) {
 				if (enfa.eClosure().contains(finals)) {
-					t=Type.FINAL;
+					t=Tip.FINAL;
 				}
 			}
 			NFAState nfaState = new NFAState(enfa.getTitle(),t);
@@ -103,11 +106,178 @@ public class TOC {
 		return NFA;
 	}
 	
-	public static void main (String [] args) {
-		//new ConversionFrame();
-		ArrayList <Character> alpha = new ArrayList<Character>();
+	public static ArrayList <DFAMinimalState> minimizeDFA (ArrayList <DFAState> DFA,ArrayList <Character> Alphabet){
+		ArrayList <DFAMinimalState> minimalDFA = new ArrayList<DFAMinimalState>();
 		
-		/*
+		int numberOfStates = DFA.size();
+		int [][] matrix = new int [numberOfStates][numberOfStates];
+		matrix = initializeMatrix(matrix,numberOfStates, DFA);
+		matrix = updateMatrix(matrix,numberOfStates,DFA, Alphabet);
+		
+		/* Afishimi i Matrices
+		for (int i=0; i<numberOfStates; i++) {
+			for (int j=0; j<numberOfStates;j++) {
+				System.out.print(matrix[i][j]+" ");
+			}
+			System.out.print("\n");
+		}
+		*/
+		
+		//ADD RESULTS FROM THE MATRIX
+		ArrayList <HashSet> results = new ArrayList <HashSet>();
+		for (int i=0; i<numberOfStates; i++) {
+			for (int j=0; j<i; j++) {
+				if (matrix[i][j]==0) {
+					HashSet<DFAState> statesPair = new HashSet<DFAState>();
+					statesPair.add(DFA.get(i));
+					statesPair.add(DFA.get(j));
+					results.add(statesPair);
+				}
+			}
+		}
+		
+		//UNITE STATES
+		for (int i=0; i<results.size();i++) {
+			HashSet <DFAState> hash1 = results.get(i);
+			for (int j=i+1; j<results.size();j++) {
+				HashSet <DFAState> hash2 = results.get(j);
+				boolean canBeUnited=false;
+				for (DFAState ds : hash2) {
+					if (hash1.contains(ds)) {
+						canBeUnited=true;
+						break;
+					}
+				}
+				if(canBeUnited) {
+					for (DFAState ds : hash2) {
+						hash1.add(ds);
+					}
+					results.remove(j);
+					j--;
+				}
+			}
+		}
+		
+		//ADD INTO RESULTS STATES OF THE LEFT-OVER DFA STATES 
+		for (DFAState ds : DFA) {
+			boolean contains = false;
+			for (HashSet<DFAState> hs : results) {
+				for (DFAState ds2 : hs) {
+					if (ds2==ds)
+						contains=true;
+				}
+			}
+			if (!contains) {
+				HashSet<DFAState> leftOverStates = new HashSet<DFAState>();
+				leftOverStates.add(ds);
+				results.add(leftOverStates);
+			}
+		} 
+		
+		//ADD STATES FROM RESULTS TO MINIMALDFA
+		for (HashSet <DFAState> hs : results) {
+			DFAMinimalState newState = new DFAMinimalState ();
+			String title="";
+			for (DFAState ds : hs) {
+				if(ds.getType()==Tip.START) {
+					newState.setType(Tip.START);
+				}
+				if (ds.getType()==Tip.FINAL) {
+					newState.setType(Tip.FINAL);
+				}
+				newState.addTitle(ds);
+			}
+			minimalDFA.add(newState);
+		}
+		
+		//LINK STATES TO ONE ANOTHER
+		for (DFAMinimalState dfms : minimalDFA) {
+			DFAState getATitle = dfms.getTitle().get(0);
+			for (char c : Alphabet) {
+				DFAState destination = getATitle.whereGoesOn(c);
+				for (DFAMinimalState dfms2 : minimalDFA) {
+					if(dfms2.containsDFAState(destination)) {
+						dfms.addTransition(c,dfms2);
+						break;
+					}
+				}
+			}
+		}
+		return minimalDFA;
+	}
+	
+	public static int[][] initializeMatrix (int [][] matrix,int size,ArrayList<DFAState> DFA) {
+		for (int i=0; i<size; i++) {
+			for (int j=0; j<=i; j++) {
+				if (i==j) {
+					matrix[i][j]=-1;
+					matrix[j][i]=-1;
+				}
+				else if (DFA.get(i).getType()==Tip.FINAL && DFA.get(j).getType()!=Tip.FINAL) {
+					matrix[i][j]=1;
+					matrix[j][i]=1;
+				}
+				else if (DFA.get(j).getType()==Tip.FINAL && DFA.get(i).getType()!=Tip.FINAL) {
+					matrix[i][j]=1;
+					matrix[j][i]=1;
+				}
+				else {
+					matrix[i][j]=0;
+					matrix[j][i]=0;
+				}
+			}
+		}
+		return matrix;
+	}
+	
+	public static int[][] updateMatrix (int [][]matrix,int size, ArrayList<DFAState>DFA, ArrayList <Character> Alphabet){
+		int q1,q2; int count=0;
+		boolean done = false;
+		boolean changed;
+		//while (!done) {
+			changed=false;
+			
+			for (int i=0; i<size; i++) {
+				for (int j=0; j<i; j++) {
+					for (char c : Alphabet) {
+					//changed - derisa te mos kemi modifikime ne matrice
+						q1 = getIndexOfState(DFA,DFA.get(i).whereGoesOn(c));
+						q2 = getIndexOfState(DFA,DFA.get(j).whereGoesOn(c));
+						//System.out.println("(q1,q2): "+"("+q1+","+q2+")");
+					
+						if (matrix[q1][q2]==1 || matrix[q2][q1]==1) {
+							matrix[i][j]=1;
+							matrix[j][i]=1;
+							changed=true;
+							
+							break;
+						}
+						
+					}
+				}
+			}
+			
+			if(!changed) 
+				done=true;
+		//}
+		return matrix;
+	}
+	
+	public static int getIndexOfState (ArrayList<DFAState>DFA, DFAState ds) {
+		for (int i=0; i<DFA.size();i++) {
+			if (DFA.get(i)==ds)
+				return i;
+		}
+		return -1;
+	}
+
+	
+	public static void main (String [] args) {
+		
+		new ConversionFrame();
+		/*ArrayList <Character> alpha = new ArrayList<Character>();
+		
+		
 		alpha.add('0');
 		alpha.add('1');
 		
@@ -129,27 +299,86 @@ public class TOC {
 		}
 		*/
 		
+		/*
+		ArrayList <Character> alpha = new ArrayList<Character>();
 		
 		alpha.add('0');
 		alpha.add('1');
 		alpha.add('2');
+		//alpha.add('2');
 
-		eNFAState q0 = new eNFAState("q0", Type.START);
-		eNFAState q1 = new eNFAState("q1",Type.NONE);
-		eNFAState q2 = new eNFAState("q2",Type.FINAL);
+		eNFAState q1 = new eNFAState("q1", Type.START);
+		eNFAState q2 = new eNFAState("q2",Type.NONE);
+		eNFAState q3 = new eNFAState("q3",Type.FINAL);
 		
-		q0.addTransition('0',q0);
-		q0.addTransition('Ɛ',q1);
-		q1.addTransition('1',q1);
+		q1.addTransition('0',q1);
 		q1.addTransition('Ɛ',q2);
-		q2.addTransition('2',q2);
-		//q2.addTransition('1',q2);
+		q2.addTransition('Ɛ',q3);
+		q2.addTransition('1',q2);
+		q3.addTransition('2',q3);
 		ArrayList <eNFAState> eNFA = new ArrayList<eNFAState>();
-		eNFA.add(q0); eNFA.add(q1); eNFA.add(q2); 
+		eNFA.add(q1); eNFA.add(q2); eNFA.add(q3); 
 		
+		
+		System.out.println("Successfully converted from eNFA to NFA");
 		for (NFAState nfa : convertToNFA(eNFA, alpha)) {
 			nfa.bridh();
 		}
-				
+		System.out.println("\n Successfully converted from NFA to DFA");
+		{
+			for (DFAState dfa : convertToDFA(convertToNFA(eNFA,alpha),alpha)) {
+				dfa.bridh();
+			}
+		}
+			
+		
+		
+		/*
+		ArrayList <Character> alpha = new ArrayList<Character>();
+		alpha.add('0');
+		alpha.add('1');
+		
+		DFAState A = new DFAState("A");
+		A.setType(Type.START);
+		DFAState B = new DFAState("B");
+		DFAState C = new DFAState("C");
+		C.setType(Type.FINAL);
+		DFAState D = new DFAState("D");
+		D.setType(Type.FINAL);
+		DFAState E = new DFAState("E");
+		E.setType(Type.FINAL);
+		DFAState F = new DFAState("F");
+		
+		A.addTransition('0',B);
+		A.addTransition('1',C);
+		
+		B.addTransition('0',A);
+		B.addTransition('1',D);
+		
+		C.addTransition('0',E);
+		C.addTransition('1',F);
+		
+		D.addTransition('0', E);
+		D.addTransition('1', F);
+		
+		E.addTransition('0', E);
+		E.addTransition('1', F);
+		
+		F.addTransition('0', F);
+		F.addTransition('1', F);
+		
+		ArrayList <DFAState> dfaEx = new ArrayList<DFAState>();
+		dfaEx.add(A); dfaEx.add(B); dfaEx.add(C); dfaEx.add(D); dfaEx.add(E); dfaEx.add(F);
+		
+		
+		for (DFAState dfa :dfaEx) {
+			dfa.bridh();
+		}
+		System.out.println("");
+		//PRINT STATES
+				for (DFAMinimalState ds : minimizeDFA(dfaEx,alpha)) {
+					ds.bridh();
+				}
+		*/
 	}
 }
