7cb225531fa38aa85120e62026a78810527b864c
Evald Nexhipi <51530364+evaldnexhipi@users.noreply.github.com>
2020-02-06 11:39:33 +0100

    Add files via upload

---
+++ DFAMinimalState.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class DFAMinimalState {
+	private ArrayList<DFAState> title;
+	private Tip type;
+	private ArrayList <Character> symbols;
+	private ArrayList <DFAMinimalState> adjacents;
+	public DFAMinimalState() {
+		title=new ArrayList<DFAState>();
+		symbols=new ArrayList<Character>();
+		adjacents=new ArrayList<DFAMinimalState>();
+		type=Tip.NONE;
+	}
+	
+	public char getSymbol(int i) {
+		return symbols.get(i);
+	}
+	
+	public DFAMinimalState getAdjacent(int i) {
+		return adjacents.get(i);
+	}
+	
+	public void setType (Tip t) {
+		type=t;
+	}
+	
+	public boolean containsDFAState(DFAState dfas) {
+		for (DFAState s : title) {
+			if(s==dfas)
+				return true;
+		}
+		return false;
+	}
+	
+	public ArrayList <DFAState> getTitle(){
+		return this.title;
+	}
+	
+	public void addTitle(DFAState ds) {
+		title.add(ds);
+	}
+	
+	public ArrayList <Character> getSymbols(){
+		return this.symbols;
+	}
+	
+	public void addTransition (char c, DFAMinimalState nextState) {
+		symbols.add(c);
+		adjacents.add(nextState);
+	}
+	
+	public void bridh () {
+		String name = toString();
+		for (int i=0; i<symbols.size();i++) {
+			System.out.println(name+"-> "+symbols.get(i)+" -> "+adjacents.get(i));
+		}
+	}
+	
+	public String toString() {
+		String s = "";
+		for (DFAState dfas : title) {
+			//s+=dfas.getTitle2()+",";
+			s+=dfas+",";
+		}
+		s=s.substring(0,s.length()-1);
+		if(this.type==Tip.FINAL)
+			s="{"+s+"}";
+		return s;
+	}
+}
